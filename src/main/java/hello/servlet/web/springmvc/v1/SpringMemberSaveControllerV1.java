c35c9e38b94e63a6fd8ee23f433a1b0701b42086
dev-lkj <2_kj@daum.net>
2024-02-09 22:51:37 +0900

    frontcontroller add

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+
+}
